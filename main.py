82cbff174c670e6a11d408a6c95bd6509e8a1ebd
rlagusdn812 <dg2026812@danggok.hs.kr>
2026-05-22 09:07:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 Web VPython 3.2
+
 sphere(size = vec(10, 10, 10), opacity = 0.3)
+
+ball = []
+
 for i in range(45):
-   b = sphere(pos =vector(i,0,0))
+
+    b = sphere(pos = vector(i * 0.1, i * 0.1, i * 0.1), radius = 0.3)
+    print(i + 1)
