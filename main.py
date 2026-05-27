3a2fa338633ebef6691f6a36590144cc9a734ce3
rlagusdn812 <dg2026812@danggok.hs.kr>
2026-05-27 11:10:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 Web VPython 3.2
 
-sphere(size = vec(10, 10, 10), opacity = 0.3)
+import random
 
-ball = []
+box = sphere(size=vector(10, 10, 10), opacity=0.3, color=color.gray(0.5))
 
-for i in range(45):
+balls = []
 
-    b = sphere(pos = vector(i * 0.1, i * 0.1, i * 0.1), radius = 0.3)
-    print(i + 1)
+for i in range(1, 46):
+
+    while True:
+        pos_vec = vector.random() 
+        if mag(pos_vec) <= 1: 
+            break
+            
+    if i <= 10: c = color.yellow
+    elif i <= 20: c = color.blue
+    elif i <= 30: c = color.red
+    elif i <= 40: c = color.black
+    else: c = color.green
+    
+    s = sphere(radius=0.3, color=c)
+    t = text(text=str(i), pos=vector(-0.15, -0.1, 0.3), height=0.2, color=color.white)
+    
+    b = compound([s, t])
+    b.pos = pos_vec
+    
+    balls.append(b)
